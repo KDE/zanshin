b8a1a8afff8616f526abb63f21a4bf428d137266
Kevin Ottens <ervin@kde.org>
2009-01-18 22:38:43 +0000

    Allow to add and remove projects. Some code dups needs cleanup.

--- sidebar.cpp
+++ sidebar.cpp
@@ -20,10 +20,22 @@
 
 #include "sidebar.h"
 
+#include <akonadi/collection.h>
+#include <akonadi/item.h>
+#include <akonadi/itemcreatejob.h>
+#include <akonadi/itemdeletejob.h>
+#include <akonadi/transactionsequence.h>
+
+#include <boost/shared_ptr.hpp>
+
+#include <kcal/todo.h>
+
 #include <KDE/KAction>
 #include <KDE/KActionCollection>
 #include <KDE/KIcon>
+#include <KDE/KInputDialog>
 #include <KDE/KLocale>
+#include <KDE/KMessageBox>
 
 #include <QtGui/QStackedWidget>
 #include <QtGui/QToolBar>
@@ -36,6 +48,8 @@
 #include "projectsmodel.h"
 #include "todoflatmodel.h"
 #include "todotreemodel.h"
+
+typedef boost::shared_ptr<KCal::Incidence> IncidencePtr;
 
 SideBar::SideBar(QWidget *parent, KActionCollection *ac)
     : QWidget(parent)
@@ -158,7 +172,7 @@ void SideBar::updateActions(const QModelIndex &index)
         m_remove->setEnabled(false);
     } else {
         m_add->setEnabled(true);
-        m_remove->setEnabled(true);
+        m_remove->setEnabled(!model->isLibraryRoot(index));
 
         QModelIndex sourceIndex = model->mapToSource(index); // into "projects"
         if (m_addFolder->isEnabled() && sourceIndex.isValid()) { // Shouldn't be enabled on projects
@@ -166,6 +180,7 @@ void SideBar::updateActions(const QModelIndex &index)
             sourceIndex = sourceIndex.sibling(sourceIndex.row(), TodoFlatModel::RowType); // we want the row type
             if (GlobalModel::todoTree()->data(sourceIndex).toInt()!=TodoFlatModel::FolderTodo) {
                 m_addFolder->setEnabled(false);
+                m_add->setEnabled(false);
             }
         }
     }
@@ -173,7 +188,37 @@ void SideBar::updateActions(const QModelIndex &index)
 
 void SideBar::onAddFolder()
 {
+    bool ok;
+    QString summary = KInputDialog::getText(i18n("New Folder"),
+                                            i18n("Enter folder name:"),
+                                            QString(), &ok, this);
+    summary = summary.trimmed();
 
+    if (!ok || summary.isEmpty()) return;
+
+    QModelIndex parent = m_projectTree->currentIndex();
+    parent = GlobalModel::projectsLibrary()->mapToSource(parent);
+    parent = GlobalModel::projects()->mapToSource(parent);
+    parent = parent.sibling(parent.row(), TodoFlatModel::RemoteId);
+
+    QString parentRemoteId = GlobalModel::todoTree()->data(parent).toString();
+
+    KCal::Todo *todo = new KCal::Todo();
+    todo->setSummary(summary);
+    todo->addComment("X-Zanshin-Folder");
+
+    if (!parentRemoteId.isEmpty()) {
+        todo->setRelatedToUid(parentRemoteId);
+    }
+
+    IncidencePtr incidence(todo);
+    Akonadi::Item item;
+    item.setMimeType("application/x-vnd.akonadi.calendar.todo");
+    item.setPayload<IncidencePtr>(incidence);
+
+    Akonadi::Collection collection = GlobalModel::todoFlat()->collection();
+    Akonadi::ItemCreateJob *job = new Akonadi::ItemCreateJob(item, collection);
+    job->start();
 }
 
 void SideBar::onAddItem()
@@ -206,12 +251,80 @@ void SideBar::onRemoveItem()
 
 void SideBar::addNewProject()
 {
+    bool ok;
+    QString summary = KInputDialog::getText(i18n("New Project"),
+                                            i18n("Enter project name:"),
+                                            QString(), &ok, this);
+    summary = summary.trimmed();
 
+    if (!ok || summary.isEmpty()) return;
+
+    QModelIndex parent = m_projectTree->currentIndex();
+    parent = GlobalModel::projectsLibrary()->mapToSource(parent);
+    parent = GlobalModel::projects()->mapToSource(parent);
+    parent = parent.sibling(parent.row(), TodoFlatModel::RemoteId);
+
+    QString parentRemoteId = GlobalModel::todoTree()->data(parent).toString();
+
+    KCal::Todo *todo = new KCal::Todo();
+    todo->setSummary(summary);
+    todo->addComment("X-Zanshin-Project");
+
+    if (!parentRemoteId.isEmpty()) {
+        todo->setRelatedToUid(parentRemoteId);
+    }
+
+    IncidencePtr incidence(todo);
+    Akonadi::Item item;
+    item.setMimeType("application/x-vnd.akonadi.calendar.todo");
+    item.setPayload<IncidencePtr>(incidence);
+
+    Akonadi::Collection collection = GlobalModel::todoFlat()->collection();
+    Akonadi::ItemCreateJob *job = new Akonadi::ItemCreateJob(item, collection);
+    job->start();
+}
+
+void addDeleteTodoJobsHelper(const QModelIndex &index, Akonadi::TransactionSequence *transaction)
+{
+    for (int i=0; i<GlobalModel::todoTree()->rowCount(index); i++) {
+        addDeleteTodoJobsHelper(index.child(i, 0), transaction);
+    }
+
+    Akonadi::Item item = GlobalModel::todoTree()->itemForIndex(index);
+    new Akonadi::ItemDeleteJob(item, transaction);
 }
 
 void SideBar::removeCurrentProject()
 {
+    QModelIndex current = m_projectTree->currentIndex();
+    current = GlobalModel::projectsLibrary()->mapToSource(current);
+    current = GlobalModel::projects()->mapToSource(current);
+    current = current.sibling(current.row(), 0);
 
+    bool canRemove = true;
+
+    if (GlobalModel::todoTree()->rowCount(current)>0) {
+        QString summary = GlobalModel::todoTree()->data(current).toString();
+        int rowType = GlobalModel::todoTree()->data(current.sibling(current.row(), TodoFlatModel::RowType)).toInt();
+        QString message;
+        QString caption;
+        if (rowType==TodoFlatModel::FolderTodo) {
+            message = i18n("Do you really want to delete the folder '%1', with all its projects and actions?", summary);
+            caption = i18n("Delete Folder");
+        } else {
+            message = i18n("Do you really want to delete the project '%1', with all its actions?", summary);
+            caption = i18n("Delete Project");
+        }
+        int button = KMessageBox::questionYesNo(this, message, caption);
+
+        canRemove = (button==KMessageBox::Yes);
+    }
+
+    if (!canRemove) return;
+
+    Akonadi::TransactionSequence *transaction = new Akonadi::TransactionSequence();
+    addDeleteTodoJobsHelper(current, transaction);
+    transaction->start();
 }
 
 void SideBar::addNewContext()
