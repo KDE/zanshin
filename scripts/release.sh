bae3adec6d89940475c5623b7d2e812ef098ac3e
Kevin Ottens <ervin@kde.org>
2010-08-23 18:33:19 +0200

    Add a release script which does the following:  * produce tarballs for the provided tags;  * build one of the tarballs;  * run the unit tests in it if available.

---
+++ scripts/release.sh
@@ -0,0 +1,65 @@
+#! /bin/sh
+
+gitarchive()
+{
+	project=$1
+	tree=$2
+	compression=$3
+	extension=$4
+
+	git archive --format=tar --prefix=$project-$tree/ $tree | $compression -c > $project-$tree.tar.$extension
+}
+
+buildarchive()
+{
+	project=$1
+	tree=$2
+
+	tar zxf $project-$tree.tar.gz \
+	&& cd $project-$tree \
+	&& mkdir build \
+	&& cd build \
+	&& cmake -DKDE4_BUILD_TESTS=ON ../ \
+	&& make $MAKEOPTS \
+	&& cd ../..
+}
+
+runtests()
+{
+	project=$1
+	tree=$2
+
+	cd $project-$tree/build \
+	&& make test \
+	&& cd ../..
+}
+
+# Looking for the .git directory to be sure we're
+# at the top level of the working directory
+
+while ! test -e ".git"; do
+	cd ..
+	if test "$PWD" = "/"; then
+		echo "We're not in a git working directory!"
+		exit 1
+	fi
+done
+
+project=`basename $PWD`
+
+
+for tree in $*; do
+	echo "Generating archives for $project $tree"
+
+	gitarchive $project $tree "gzip" "gz"
+	gitarchive $project $tree "bzip2" "bz2"
+
+	echo "Building"
+	buildarchive $project $tree "tar.gz" || exit 1
+
+	echo "Testing"
+	runtests $project $tree "tar.gz" || exit 1
+
+	echo "Cleanup"
+	rm -r ./$project-$tree
+done;
