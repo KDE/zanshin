7078ae778af7b243f7549831fe6ccdea88c848c8
Mario Bensi <mbensi@ipsquad.net>
2011-05-12 18:08:36 +0200

    Add test for AncestorUid role on TodoMetadataModel

--- tests/todometadatamodelspec.cpp
+++ tests/todometadatamodelspec.cpp
@@ -224,6 +224,109 @@ private slots:
 
         QCOMPARE(index.data(Zanshin::ParentUidRole).toString(), parentUid);
     }
+
+    void shouldRetrieveItemAncestorsUid_data()
+    {
+        QTest::addColumn<ModelStructure>( "sourceStructure" );
+        QTest::addColumn<QStringList>( "ancestors" );
+
+        C c1(1, 0, "c1");
+        T t1(1, 1, "t1", QString(), "t1");
+        T t2(2, 1, "t2", "t1", "t2");
+        T t3(3, 2, "t3", "t2", "t3");
+        T t4(4, 3, "t4", "t3", "t4");
+        T t5(5, 4, "t5", "t4", "t5");
+        T t6(6, 5, "t6", "t5", "t6");
+        T t7(7, 6, "t7", "t6", "t7");
+        T t8(8, 7, "t8", "t7", "t8");
+        T t9(9, 8, "t9", "t8", "t9");
+
+        ModelStructure sourceStructure;
+        sourceStructure << c1;
+
+        QStringList ancestors;
+        QTest::newRow( "check ancestor on collection" ) << sourceStructure << ancestors;
+
+        sourceStructure.clear();
+        sourceStructure << c1
+                        << _+t1;
+
+        QTest::newRow( "check ancestor with collection as parent" ) << sourceStructure << ancestors;
+
+        sourceStructure.clear();
+        sourceStructure << c1
+                        << _+t1
+                        << _+t2;
+
+        ancestors << "t1";
+
+        QTest::newRow( "check ancestor with one level" ) << sourceStructure << ancestors;
+
+        sourceStructure.clear();
+        sourceStructure << c1
+                        << _+t1
+                        << _+t2
+                        << _+t3;
+
+        ancestors.clear();
+        ancestors << "t2"
+                  << "t1";
+
+        QTest::newRow( "check ancestor with two level" ) << sourceStructure << ancestors;
+
+        sourceStructure.clear();
+        sourceStructure << c1
+                        << _+t1
+                        << _+t2
+                        << _+t3
+                        << _+t4
+                        << _+t5
+                        << _+t6
+                        << _+t7
+                        << _+t8
+                        << _+t9;
+
+        ancestors.clear();
+        ancestors << "t8"
+                  << "t7"
+                  << "t6"
+                  << "t5"
+                  << "t4"
+                  << "t3"
+                  << "t2"
+                  << "t1";
+
+        QTest::newRow( "check ancestor with height level" ) << sourceStructure << ancestors;
+    }
+
+    void shouldRetrieveItemAncestors()
+    {
+        //GIVEN
+        QFETCH(ModelStructure, sourceStructure);
+
+        //Source model
+        QStandardItemModel source;
+        StandardModelBuilderBehavior behavior;
+        behavior.setMetadataCreationEnabled(false);
+        ModelUtils::create(&source, sourceStructure, ModelPath(), &behavior);
+
+        //create metadataModel
+        TodoMetadataModel todoMetadataModel;
+        ModelTest t1(&todoMetadataModel);
+
+        //WHEN
+        todoMetadataModel.setSourceModel(&source);
+
+        //THEN
+        QFETCH(QStringList, ancestors);
+        QModelIndex index = todoMetadataModel.index(0,0);
+        while (todoMetadataModel.rowCount(index) > 0) {
+            index = todoMetadataModel.index(todoMetadataModel.rowCount(index)-1, 0, index);
+        }
+
+        QCOMPARE(index.data(Zanshin::AncestorsUidRole).toStringList(), ancestors);
+    }
+
 };
 
 QTEST_KDEMAIN(TodoMetadataModelTest, GUI)
