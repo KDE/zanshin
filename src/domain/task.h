f03a00a556a54dbf1cbc327879e627e2f5c6a962
Kevin Ottens <ervin@kde.org>
2014-01-13 20:36:35 +0100

    Add a Task class to the Domain

---
+++ src/domain/task.h
@@ -0,0 +1,70 @@
+/* This file is part of Zanshin Todo.
+
+   Copyright 2011 Christian Mollekopf <chrigi_1@fastmail.fm>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of
+   the License or (at your option) version 3 or any later version
+   accepted by the membership of KDE e.V. (or its successor approved
+   by the membership of KDE e.V.), which shall act as a proxy
+   defined in Section 14 of version 3 of the license.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+   USA.
+*/
+
+
+#ifndef DOMAIN_TASK_H
+#define DOMAIN_TASK_H
+
+#include "artifact.h"
+#include <QDateTime>
+
+namespace Domain {
+
+class Task : public Artifact
+{
+    Q_OBJECT
+    Q_PROPERTY(bool done READ isDone WRITE setDone NOTIFY doneChanged)
+    Q_PROPERTY(QDateTime startDate READ startDate WRITE setStartDate NOTIFY startDateChanged)
+    Q_PROPERTY(QDateTime dueDate READ dueDate WRITE setDueDate NOTIFY dueDateChanged)
+
+public:
+    typedef QSharedPointer<Task> Ptr;
+
+    Task(QObject *parent = 0);
+    virtual ~Task();
+
+    bool isDone() const;
+    QDateTime startDate() const;
+    QDateTime dueDate() const;
+
+public slots:
+    void setDone(bool done);
+    void setStartDate(const QDateTime &startDate);
+    void setDueDate(const QDateTime &dueDate);
+
+signals:
+    void doneChanged(bool isDone);
+    void startDateChanged(const QDateTime &startDate);
+    void dueDateChanged(const QDateTime &dueDate);
+
+private:
+    bool m_done;
+    QDateTime m_startDate;
+    QDateTime m_dueDate;
+};
+
+}
+
+Q_DECLARE_METATYPE(Domain::Task::Ptr)
+
+#endif // DOMAIN_TASK_H
---
+++ tests/units/tasktest.cpp
@@ -0,0 +1,101 @@
+/* This file is part of Zanshin Todo.
+
+   Copyright 2014 Kevin Ottens <ervin@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of
+   the License or (at your option) version 3 or any later version
+   accepted by the membership of KDE e.V. (or its successor approved
+   by the membership of KDE e.V.), which shall act as a proxy
+   defined in Section 14 of version 3 of the license.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+   USA.
+*/
+
+#include <QtTest>
+
+#include "domain/task.h"
+
+using namespace Domain;
+
+class TaskTest : public QObject
+{
+    Q_OBJECT
+private slots:
+    void shouldHaveEmptyPropertiesByDefault()
+    {
+        Task t;
+        QCOMPARE(t.text(), QString());
+        QCOMPARE(t.title(), QString());
+        QCOMPARE(t.isDone(), false);
+        QCOMPARE(t.startDate(), QDateTime());
+        QCOMPARE(t.dueDate(), QDateTime());
+    }
+
+    void shouldNotifyStatusChanges()
+    {
+        Task t;
+        QSignalSpy spy(&t, SIGNAL(doneChanged(bool)));
+        t.setDone(true);
+        QCOMPARE(spy.count(), 1);
+        QCOMPARE(spy.first().first().toBool(), true);
+    }
+
+    void shouldNotNotifyIdenticalStatusChanges()
+    {
+        Task t;
+        t.setDone(true);
+        QSignalSpy spy(&t, SIGNAL(doneChanged(bool)));
+        t.setDone(true);
+        QCOMPARE(spy.count(), 0);
+    }
+
+    void shouldNotifyStartDateChanges()
+    {
+        Task t;
+        QSignalSpy spy(&t, SIGNAL(startDateChanged(QDateTime)));
+        t.setStartDate(QDateTime(QDate(2014, 1, 13)));
+        QCOMPARE(spy.count(), 1);
+        QCOMPARE(spy.first().first().toDateTime(), QDateTime(QDate(2014, 1, 13)));
+    }
+
+    void shouldNotNotifyIdenticalStartDateChanges()
+    {
+        Task t;
+        t.setStartDate(QDateTime(QDate(2014, 1, 13)));
+        QSignalSpy spy(&t, SIGNAL(startDateChanged(QDateTime)));
+        t.setStartDate(QDateTime(QDate(2014, 1, 13)));
+        QCOMPARE(spy.count(), 0);
+    }
+
+    void shouldNotifyDueDateChanges()
+    {
+        Task t;
+        QSignalSpy spy(&t, SIGNAL(dueDateChanged(QDateTime)));
+        t.setDueDate(QDateTime(QDate(2014, 1, 13)));
+        QCOMPARE(spy.count(), 1);
+        QCOMPARE(spy.first().first().toDateTime(), QDateTime(QDate(2014, 1, 13)));
+    }
+
+    void shouldNotNotifyIdenticalDueDateChanges()
+    {
+        Task t;
+        t.setDueDate(QDateTime(QDate(2014, 1, 13)));
+        QSignalSpy spy(&t, SIGNAL(dueDateChanged(QDateTime)));
+        t.setDueDate(QDateTime(QDate(2014, 1, 13)));
+        QCOMPARE(spy.count(), 0);
+    }
+};
+
+QTEST_MAIN(TaskTest)
+
+#include "tasktest.moc"
