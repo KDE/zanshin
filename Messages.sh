97dd14599bc498b0d5f9e2456487a721bd7528ab
Pino Toscano <pino@kde.org>
2014-03-24 09:20:23 +0100

    i18n: do not extract messages from 'tests' subdirs

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc` >> rc.cpp
-$XGETTEXT `find . -name \*.h -o -name \*.cpp` -o $podir/zanshin.pot
+$XGETTEXT `find . -name \*.h -o -name \*.cpp | grep -v '/tests/'` -o $podir/zanshin.pot
 rm -f rc.cpp
