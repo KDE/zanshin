746550ebafdc6b670488e0beecba1aa43af63ada
Kevin Ottens <ervin@kde.org>
2014-03-28 12:19:15 +0100

    Add a small script to run a few quality checks locally
    It depends on makeobj and cppcheck. Probably a good idea to use those
    anyway, and run this script before a push. We can probably add more
    checks later on.
    
    CCMAIL: zanshin-devel@kde.org

---
+++ scripts/qualitycheck.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+# Looking for the .git directory to be sure we're
+# at the top level of the working directory
+
+while ! test -e ".git"; do
+	cd ..
+	if test "$PWD" = "/"; then
+		echo "We're not in a git working directory!"
+		exit 1
+	fi
+done
+
+project=`basename $PWD`
+
+
+echo "Starting quality checks for $project"
+
+echo "Automated Tests"
+makeobj test || exit 1
+
+echo "CppCheck"
+cppcheck --quiet --enable=style --enable=performance --enable=portability --enable=information --enable=missingInclude --inline-suppr .
