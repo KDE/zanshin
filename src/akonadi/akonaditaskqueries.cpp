868c6d954230f75d4c1c4ad37d643caf6ad70699
Mario Bensi <mbensi@ipsquad.net>
2014-04-02 15:54:41 +0200

    Test ChildProvider in onItemRemoved
    Remove task in childProvider on remove notification.

--- src/akonadi/akonaditaskqueries.cpp
+++ src/akonadi/akonaditaskqueries.cpp
@@ -179,6 +179,26 @@ void TaskQueries::onItemRemoved(const Item &item)
             }
         }
     }
+
+    if (m_taskChildProviders.isEmpty())
+        return;
+
+    auto uid = m_serializer->relatedUidFromItem(item);
+    if (m_uidtoIdCache.contains(uid)) {
+        auto parentId = m_uidtoIdCache.value(uid);
+        if (m_taskChildProviders.contains(parentId)) {
+            TaskProvider::Ptr childProvider(m_taskChildProviders.value(parentId).toStrongRef());
+            if (childProvider) {
+                for (int i = 0; i < childProvider->data().size(); i++) {
+                    auto task = childProvider->data().at(i);
+                    if (isTaskItem(task, item)) {
+                        childProvider->removeAt(i);
+                        i--;
+                    }
+                }
+            }
+        }
+    }
 }
 
 void TaskQueries::onItemChanged(const Item &item)
