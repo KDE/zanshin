15065fd14ec1dd7a83fae9eb2f8781c4efe77aa3
Kevin Ottens <ervin@kde.org>
2014-07-28 17:05:23 +0200

    Move QueryResultInputImpl with QueryResultProvider where it belongs
    Now queryresultprovider.h doesn't need to include queryresult.h anymore.
    We have a much better dependency graph.

--- src/domain/queryresultprovider.h
+++ src/domain/queryresultprovider.h
@@ -30,9 +30,90 @@
 #include <QList>
 #include <QSharedPointer>
 
-#include "queryresult.h"
-
 namespace Domain {
+
+template<typename ItemType>
+class QueryResultProvider;
+
+template<typename InputType>
+class QueryResultInputImpl
+{
+public:
+    typedef typename QueryResultProvider<InputType>::Ptr ProviderPtr;
+    typedef QSharedPointer<QueryResultInputImpl<InputType> > Ptr;
+    typedef QWeakPointer<QueryResultInputImpl<InputType>> WeakPtr;
+    typedef std::function<void(InputType, int)> ChangeHandler;
+    typedef QList<ChangeHandler> ChangeHandlerList;
+
+    virtual ~QueryResultInputImpl() {}
+
+protected:
+    explicit QueryResultInputImpl(const ProviderPtr &provider)
+        : m_provider(provider)
+    {
+    }
+
+    static void registerResult(const ProviderPtr &provider, const Ptr &result)
+    {
+        provider->m_results << result;
+    }
+
+    static ProviderPtr retrieveProvider(const Ptr &result)
+    {
+        return result->m_provider;
+    }
+
+    // cppcheck can't figure out the friend class
+    // cppcheck-suppress unusedPrivateFunction
+    ChangeHandlerList preInsertHandlers() const
+    {
+        return m_preInsertHandlers;
+    }
+
+    // cppcheck can't figure out the friend class
+    // cppcheck-suppress unusedPrivateFunction
+    ChangeHandlerList postInsertHandlers() const
+    {
+        return m_postInsertHandlers;
+    }
+
+    // cppcheck can't figure out the friend class
+    // cppcheck-suppress unusedPrivateFunction
+    ChangeHandlerList preRemoveHandlers() const
+    {
+        return m_preRemoveHandlers;
+    }
+
+    // cppcheck can't figure out the friend class
+    // cppcheck-suppress unusedPrivateFunction
+    ChangeHandlerList postRemoveHandlers() const
+    {
+        return m_postRemoveHandlers;
+    }
+
+    // cppcheck can't figure out the friend class
+    // cppcheck-suppress unusedPrivateFunction
+    ChangeHandlerList preReplaceHandlers() const
+    {
+        return m_preReplaceHandlers;
+    }
+
+    // cppcheck can't figure out the friend class
+    // cppcheck-suppress unusedPrivateFunction
+    ChangeHandlerList postReplaceHandlers() const
+    {
+        return m_postReplaceHandlers;
+    }
+
+    friend class QueryResultProvider<InputType>;
+    ProviderPtr m_provider;
+    ChangeHandlerList m_preInsertHandlers;
+    ChangeHandlerList m_postInsertHandlers;
+    ChangeHandlerList m_preRemoveHandlers;
+    ChangeHandlerList m_postRemoveHandlers;
+    ChangeHandlerList m_preReplaceHandlers;
+    ChangeHandlerList m_postReplaceHandlers;
+};
 
 template<typename ItemType>
 class QueryResultProvider
