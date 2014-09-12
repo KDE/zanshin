e22bd83f96ffc8ceeadd57fbd1d6e9e724cee666
David Faure <faure@kde.org>
2014-09-12 18:36:01 +0200

    Move akonaditest-run.sh.in to a shared location

--- tests/testlib/MacroAkonadiAutoTests.cmake
+++ tests/testlib/MacroAkonadiAutoTests.cmake
@@ -1,3 +1,5 @@
+set(_akonaditest_source_dir ${CMAKE_CURRENT_LIST_DIR})
+
 MACRO(ZANSHIN_AKONADI_AUTO_TESTS)
   FOREACH(_testname ${ARGN})
     kde4_add_executable(${_testname} TEST ${_testname}.cpp)
@@ -20,7 +22,7 @@ MACRO(ZANSHIN_AKONADI_AUTO_TESTS)
 
     set(AKONADITEST_CWD ${CMAKE_CURRENT_SOURCE_DIR})
     set(zanshin-testcommand "${_executable} 2>&1")
-    configure_file(akonaditest-run.sh.in ${_testname}-run.sh)
+    configure_file(${_akonaditest_source_dir}/akonaditest-run.sh.in ${_testname}-run.sh)
 
     set(akonaditest-run-cmd ${_testrunner} -c ${CMAKE_CURRENT_SOURCE_DIR}/testenv/config.xml
                             sh ${CMAKE_CURRENT_BINARY_DIR}/${_testname}-run.sh 2> ${CMAKE_CURRENT_BINARY_DIR}/akonaditest-${_testname}.log)
