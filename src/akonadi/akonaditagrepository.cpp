7c957170067b2d5c284566afac6bcc0f372e1647
Franck Arrecot <franck.arrecot@kdab.com>
2014-10-23 19:36:37 +0200

    AkonadiTagRepository class skeleton and its skeleton test
    REVIEW: 120759

---
+++ src/akonadi/akonaditagrepository.cpp
@@ -0,0 +1,81 @@
+/* This file is part of Zanshin
+
+   Copyright 2014 Kevin Ottens <ervin@kde.org>
+   Copyright 2014 Franck Arrecot <franck.arrecot@gmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of
+   the License or (at your option) version 3 or any later version
+   accepted by the membership of KDE e.V. (or its successor approved
+   by the membership of KDE e.V.), which shall act as a proxy
+   defined in Section 14 of version 3 of the license.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+   USA.
+*/
+
+#include "akonaditagrepository.h"
+
+#include "akonadiitemfetchjobinterface.h"
+#include "akonadiserializer.h"
+#include "akonadistorage.h"
+
+#include "utils/compositejob.h"
+
+using namespace Akonadi;
+
+
+TagRepository::TagRepository(QObject *parent)
+    : QObject(parent),
+      m_storage(new Storage),
+      m_serializer(new Serializer),
+      m_ownInterfaces(true)
+{
+}
+
+TagRepository::TagRepository(StorageInterface *storage, SerializerInterface *serializer)
+    : m_storage(storage),
+      m_serializer(serializer),
+      m_ownInterfaces(false)
+{
+}
+
+TagRepository::~TagRepository()
+{
+    if (m_ownInterfaces) {
+        delete m_storage;
+        delete m_serializer;
+    }
+}
+
+KJob *TagRepository::create(Domain::Tag::Ptr tag)
+{
+    qFatal("not impl yet");
+    return 0;
+}
+
+KJob *TagRepository::remove(Domain::Tag::Ptr tag)
+{
+    qFatal("not impl yet");
+    return 0;
+}
+
+KJob *TagRepository::associate(Domain::Tag::Ptr parent, Domain::Artifact::Ptr child)
+{
+    qFatal("not impl yet");
+    return 0;
+}
+
+KJob *TagRepository::dissociate(Domain::Tag::Ptr parent, Domain::Artifact::Ptr child)
+{
+    qFatal("not impl yet");
+    return 0;
+}
---
+++ src/akonadi/akonaditagrepository.h
@@ -0,0 +1,55 @@
+/* This file is part of Zanshin
+
+   Copyright 2014 Kevin Ottens <ervin@kde.org>
+   Copyright 2014 Franck Arrecot<franck.arrecot@kgmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of
+   the License or (at your option) version 3 or any later version
+   accepted by the membership of KDE e.V. (or its successor approved
+   by the membership of KDE e.V.), which shall act as a proxy
+   defined in Section 14 of version 3 of the license.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+   USA.
+*/
+
+#ifndef AKONADITAGREPOSITORY_H
+#define AKONADITAGREPOSITORY_H
+
+#include "domain/tagrepository.h"
+
+namespace Akonadi {
+
+class SerializerInterface;
+class StorageInterface;
+
+class TagRepository : public QObject, public Domain::TagRepository
+{
+    Q_OBJECT
+public:
+    explicit TagRepository(QObject* parent = 0);
+    TagRepository(StorageInterface *storage, SerializerInterface *serializer);
+    virtual ~TagRepository();
+
+    KJob *create(Domain::Tag::Ptr tag) Q_DECL_OVERRIDE;
+    KJob *remove(Domain::Tag::Ptr tag) Q_DECL_OVERRIDE;
+
+    KJob *associate(Domain::Tag::Ptr parent, Domain::Artifact::Ptr child) Q_DECL_OVERRIDE;
+    KJob *dissociate(Domain::Tag::Ptr parent, Domain::Artifact::Ptr child) Q_DECL_OVERRIDE;
+
+private:
+    StorageInterface *m_storage;
+    SerializerInterface *m_serializer;
+    bool m_ownInterfaces;
+};
+}
+#endif // AKONADITAGREPOSITORY_H
