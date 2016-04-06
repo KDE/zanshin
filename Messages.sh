11ebdfee8d99349603ecda92cdfd98db048c5f73
Montel Laurent <montel@kde.org>
2016-04-06 06:47:11 +0200

    For the moment no translation was extracted as zanshin used kde extract script.
    We need to extract string from ui file too.
    
    Differential Revision: https://phabricator.kde.org/D1322

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc` >> rc.cpp
-$XGETTEXT `find . -name \*.h -o -name \*.cpp | grep -v '/tests/'` -o $podir/zanshin.pot
+$EXTRACT_TR_STRINGS `find . -name \*.h -o -name \*.cpp -o -name \*.ui | grep -v '/tests/'` -o $podir/zanshin_qt.pot
 rm -f rc.cpp
