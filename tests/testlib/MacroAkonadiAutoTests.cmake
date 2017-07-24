e4b2456c6201b153786544566a3dd700a1194d06
Kevin Ottens <ervin@kde.org>
2017-07-24 11:50:08 +0200

    Add missing quotes to trigger akonaditest

--- tests/testlib/MacroAkonadiAutoTests.cmake
+++ tests/testlib/MacroAkonadiAutoTests.cmake
@@ -14,21 +14,21 @@ MACRO(ZANSHIN_AKONADI_AUTO_TESTS)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${KDE4_ENABLE_EXCEPTIONS}")
     target_link_libraries(${_prefixed_testname} Qt5::Test akonadi domain utils)
 
-    set(_location ${CMAKE_CURRENT_BINARY_DIR}/${_prefixed_testname})
+    set(_location "${CMAKE_CURRENT_BINARY_DIR}/${_prefixed_testname}")
     if (WIN32)
-      set(_executable ${_location}.exe)
+      set(_executable "${_location}.exe")
     else (WIN32)
-      set(_executable ${_location})
+      set(_executable "${_location}")
     endif (WIN32)
 
     find_program(_testrunner akonaditest)
 
-    set(AKONADITEST_CWD ${CMAKE_CURRENT_SOURCE_DIR})
+    set(AKONADITEST_CWD "${CMAKE_CURRENT_SOURCE_DIR}")
     set(zanshin-testcommand "${_executable} 2>&1")
     configure_file(${_akonaditest_source_dir}/akonaditest-run.sh.in ${_prefixed_testname}-run.sh)
 
-    set(akonaditest-run-cmd ${_testrunner} -c ${CMAKE_CURRENT_SOURCE_DIR}/testenv/config.xml
-                            sh ${CMAKE_CURRENT_BINARY_DIR}/${_prefixed_testname}-run.sh 2> ${CMAKE_CURRENT_BINARY_DIR}/akonaditest-${_prefixed_testname}.log)
+    set(akonaditest-run-cmd "${_testrunner}" -c "${CMAKE_CURRENT_SOURCE_DIR}/testenv/config.xml"
+                            sh "${CMAKE_CURRENT_BINARY_DIR}/${_prefixed_testname}-run.sh" 2> "${CMAKE_CURRENT_BINARY_DIR}/akonaditest-${_prefixed_testname}.log")
 
     add_custom_target(${_prefixed_testname}-run
                       COMMAND ${akonaditest-run-cmd}
@@ -36,6 +36,6 @@ MACRO(ZANSHIN_AKONADI_AUTO_TESTS)
                       USES_TERMINAL
     )
 
-    add_test(${_prefixed_testname} ${CMAKE_MAKE_PROGRAM} -C ${CMAKE_BINARY_DIR} ${_prefixed_testname}-run)
+    add_test(${_prefixed_testname} ${CMAKE_MAKE_PROGRAM} -C "${CMAKE_BINARY_DIR}" ${_prefixed_testname}-run)
   ENDFOREACH(_testname)
 ENDMACRO(ZANSHIN_AKONADI_AUTO_TESTS)
