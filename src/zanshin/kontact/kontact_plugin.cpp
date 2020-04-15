faba24de5cc556870991fcc013a0ef2dceb5eeb9
David Faure <faure@kde.org>
2020-04-15 09:29:42 +0200

    Install the kontact plugin into kontact5, with JSON metadata.
    Summary: This will allow kontact to use KPluginLoader one day.
    
    Test Plan:
    rm $prefix/lib64/plugins/kontact_zanshinplugin.so ; make install
    kontact still finds the zanshin plugin
    
    The same steps (other than old-kontact-interface compatibility) were already
    pushed to all other kontact plugins.
    
    Reviewers: ervin
    
    Reviewed By: ervin
    
    Differential Revision: https://phabricator.kde.org/D28847

--- src/zanshin/kontact/kontact_plugin.cpp
+++ src/zanshin/kontact/kontact_plugin.cpp
@@ -25,7 +25,25 @@
 
 #include <KontactInterface/Core>
 
-EXPORT_KONTACT_PLUGIN(Plugin, zanshin)
+#if KONTACTINTERFACE_VERSION < QT_VERSION_CHECK(5, 14, 42)
+/**
+  Exports Kontact plugin.
+  @param pluginclass the class to instanciate (must derive from KontactInterface::Plugin
+  @param jsonFile filename of the JSON file, generated from a .desktop file
+ */
+#define EXPORT_KONTACT_PLUGIN_WITH_JSON( pluginclass, jsonFile ) \
+    class Instance                                           \
+    {                                                        \
+    public:                                                \
+        static QObject *createInstance( QWidget *, QObject *parent, const QVariantList &list ) \
+        { return new pluginclass( static_cast<KontactInterface::Core*>( parent ), list ); } \
+    };                                                                    \
+    K_PLUGIN_FACTORY_WITH_JSON( KontactPluginFactory, jsonFile, registerPlugin< pluginclass >   \
+                              ( QString(), Instance::createInstance ); ) \
+    K_EXPORT_PLUGIN_VERSION(KONTACT_PLUGIN_VERSION)
+#endif
+
+EXPORT_KONTACT_PLUGIN_WITH_JSON(Plugin, "zanshin_plugin.json")
 
 Plugin::Plugin(KontactInterface::Core *core, const QVariantList&)
     : KontactInterface::Plugin(core, core, "zanshin")
