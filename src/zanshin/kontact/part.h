d7d4825da947721904e0e41e74a12e85da3434cd
Laurent Montel <montel@kde.org>
2023-03-30 21:11:10 +0200

    Make compile against last kf6

--- src/zanshin/kontact/part.h
+++ src/zanshin/kontact/part.h
@@ -14,7 +14,11 @@ class Part : public KParts::ReadOnlyPart
     Q_OBJECT
 
 public:
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     Part(QWidget *parentWidget, QObject *parent, const QVariantList &);
+#else
+    Part(QWidget *parentWidget, QObject *parent, const KPluginMetaData &data, const QVariantList &);
+#endif
     ~Part();
 
 protected:
