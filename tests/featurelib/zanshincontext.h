a696831625dfb11a7bff46cd2c40076c242067b1
Laurent Montel <montel@kde.org>
2023-07-02 07:54:49 +0200

    Add missing #ifndef

--- tests/featurelib/zanshincontext.h
+++ tests/featurelib/zanshincontext.h
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
  */
 
+#ifndef ZANSHINCONTEXT_H
+#define ZANSHINCONTEXT_H
 
 #include <QObject>
 #include <QPersistentModelIndex>
@@ -118,3 +120,4 @@ private:
     MonitorSpy *m_monitorSpy;
     FakeErrorHandler m_errorHandler;
 };
+#endif
