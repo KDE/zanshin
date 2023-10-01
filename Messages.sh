50c4baf5691aabfff81e3c065f784fb460a9cc43
Laurent Montel <montel@kde.org>
2023-10-01 13:43:51 +0200

    Remove *.cc as we don't have it in source code

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name '*.ui' -or -name '*.rc' -or -name '*.kcfg'` >> rc.cpp
-$XGETTEXT `find . -name '*.cpp' -or -name '*.cc' -or -name '*.h' | grep -v '/tests/'` -o $podir/zanshin.pot
+$XGETTEXT `find . -name '*.cpp' -or -name '*.h' | grep -v '/tests/'` -o $podir/zanshin.pot
 rm -f rc.cpp
