c0a9df21ae8bb0a13a0076e1424090f9fedd14e1
Laurent Montel <montel@kde.org>
2023-10-25 07:12:25 +0200

    GIT_SILENT: use KLocalizedString::setApplicationDomain(QByteArrayLiteral => saving one deep data copy

--- src/zanshin/app/main.cpp
+++ src/zanshin/app/main.cpp
@@ -37,7 +37,7 @@
 
 int main(int argc, char **argv)
 {
-    KLocalizedString::setApplicationDomain("zanshin");
+    KLocalizedString::setApplicationDomain(QByteArrayLiteral("zanshin"));
     QApplication app(argc, argv);
     Integration::initializeGlobalAppDependencies();
 
